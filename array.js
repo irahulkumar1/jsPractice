24a8c781520d39fa89f77d4a61108bc1afdec42f
Rahul Kumar <irahulkv@outlook.com>
2023-11-08 02:36:05 +0000

    pakage update

--- array.js
+++ array.js
@@ -3,6 +3,6 @@ const myArr = [1, 2, 2, 34, 5, 6];
 // console.log(myArr[4])
 // myArr.push(4)
 // myArr.push(7)
-myArr.pop()
-myArr.includes(3)
-console.log(myArr); 
+// myArr.pop()
+// myArr
+console.log(myArr.includes(3)); 
