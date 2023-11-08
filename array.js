ea37a265c086f78017bd203177b0db4c062dfcaa
Rahul Kumar <irahulkv@outlook.com>
2023-11-08 02:31:57 +0000

    array practice

--- array.js
+++ array.js
@@ -1,5 +1,8 @@
 const myArr = [1, 2, 2, 34, 5, 6];
 
 // console.log(myArr[4])
-myArr.push[4]
-console.log(myArr);
+// myArr.push(4)
+// myArr.push(7)
+myArr.pop()
+myArr.includes(3)
+console.log(myArr); 
