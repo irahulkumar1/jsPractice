64e61d679851209e4170727bbdc45cc64a889d0d
Rahul Kumar <irahulkv@outlook.com>
2023-11-14 19:04:40 +0000

    added funciton 02

---
+++ functions02.js
@@ -0,0 +1,23 @@
+// funcitons 02 
+
+// function restOperator(num1, num2, ...num){
+// return num 
+// }
+// console.log(restOperator(200,22, 45, 343, 989))
+
+// const user = {
+//     userName : "rahul",
+//     price: 999
+// }
+// function greetWithPrice (userDetail){
+//     console.log( `Hi ${userDetail.userName} and the price is ${userDetail.price}`)
+// }
+// greetWithPrice(user )
+
+// now accessing array in the function 
+// const myArray = [200, 290, 499, 2029];
+
+// function findMyArray (myArray){
+// return myArray[2]
+// }
+// console.log(findMyArray(myArray));
