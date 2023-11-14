965d8c85b7e7c6d0395e5715f7965c8ba872e688
Rahul Kumar <irahulkv@outlook.com>
2023-11-14 18:19:08 +0000

    functions basics

--- functions.js
+++ functions.js
@@ -1,1 +1,40 @@
 // functions
+
+// console.log("R");
+// console.log("a");
+// console.log("h");
+// console.log("u");
+// console.log("l");
+
+function callName() {
+  console.log("R");
+  console.log("a");
+  console.log("h");
+  console.log("u");
+  console.log("l");
+}
+// callName()
+
+// function addTwoNum(num1, num2){
+//   console.log( num1 + num2 )
+// }
+// addTwoNum(1,2);
+// addTwoNum(1,"2");
+// addTwoNum(1,"a")
+
+// function addTwoNum(num1, num2){
+//    return( num1 + num2 )
+//   }
+//  const result = addTwoNum(1,2);
+//   addTwoNum(1,"2");
+//   addTwoNum(1,"a");
+//   console.log(result);
+
+function loggedInuser(userName = "guest") {
+  if (!userName) {
+    console.log(`Please enter a user name`);
+    return
+  }
+  return `${userName} , how are you?`;
+}
+console.log(loggedInuser());
