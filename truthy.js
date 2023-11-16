c26690146acf5084d7a67ae85f6eab7e866adeb7
Rahul Kumar <irahulkv@outlook.com>
2023-11-16 19:29:38 +0000

    truthy values

--- truthy.js
+++ truthy.js
@@ -7,3 +7,43 @@ if(userEmail){
 }else{
     console.log("Don't have user email")
 }
+
+/*
+Falsy value
+
+null, undefined, 0, -0, BigInt, false, "" empty sting, NaN,
+
+
+Truty Values
+everything else is true 
+- "0", "false ", " " space within string is also true, [], {},
+- function(){} empty function is also a truthy value
+*/
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty")
+// }
+
+// const emptyObj = {};
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("object is empty",emptyObj)
+// }
+
+// nullish Coalescing Operator   (??)
+
+let val1;
+
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 10;
+
+// console.log(val1);
+
+// Ternary Operator 
+
+// Condition  ? truthy : falsy;
+
+let teaPrice = 100;
+
+teaPrice <= 80 ? console.log("less than 80") : console.log("more than 80 ");
