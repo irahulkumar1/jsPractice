33569eb0b4a69cdcf00074340e2f1670f84de5a6
Rahul Kumar <irahulkv@outlook.com>
2023-11-18 09:20:18 +0000

    code commented

--- loops.js
+++ loops.js
@@ -2,7 +2,14 @@
 
 // For loop
 // for(let){
-// for (condition){
-//     console.log("loop code executed")
-// }
-// }
+
+// Itterations 
+
+// for( let i = 0  ; i <= 10; i++ ){
+//    const element =(i);
+//   if(element === 5){
+//     console.log(element,"this is best")
+//      };
+//     console.log(element);
+// };
+//
