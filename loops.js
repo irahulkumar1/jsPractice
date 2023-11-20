61eea27f9ba0af80d9016e86397e7d713cb34954
Rahul Kumar emp0123 <115157250+emp0123@users.noreply.github.com>
2023-11-21 00:24:27 +0530

    mathematical Table

--- loops.js
+++ loops.js
@@ -14,6 +14,12 @@
 // };
 //
 
-for( let i = 0  ; i <= 10; i++ ){
-  for(let j = 0)
+// Mathematical Table with for loops
+for (let i = 0; i <= 10; i++) {
+    console.log(`Table of ${i}`)
+    for (let j = 0; j <= 10; j++
+    ) {
+        console.log(`${i} * ${j} = ${i * j}`)
+    }
+
 };
