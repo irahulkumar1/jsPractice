1cc9093f750fca14bf20a08cf956831af71e9af9
Rahul Kumar <irahulkv@outlook.com>
2023-11-24 11:06:38 +0000

    map in object

---
+++ 08_objLoop.js
@@ -0,0 +1,28 @@
+const myObj = {
+    key : "value",
+    js : "javaScript",
+    cpp : "c++",
+    py : "paython"
+}
+
+for ( const key in myObj){
+    // console.log(`${key} is the shotrtcut of ${myObj[key]}`)
+}
+
+// what if we want to use for in with array 
+
+const myArray = ["paython","javaSctipt", "c++", "java", "Django"]
+
+for (const key in myArray){
+    // console.log(myArray[key]);
+}
+
+const maps = new Map();
+maps.set("IN", "India");
+maps.set("UN", "United State");
+maps.set("Pak", "Pakistan");
+maps.set("Pak", "Pakistan");
+
+for ( const key in maps){
+    console.log(key)
+}
