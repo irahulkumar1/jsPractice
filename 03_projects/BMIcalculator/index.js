6c7516701b70e5f660d9dee9427f6bba74154b3c
Rahul Kumar <irahulkv@outlook.com>
2023-11-29 18:23:48 +0000

    BMI calculator finshed: learned DOM manipulation

---
+++ 03_projects/BMIcalculator/index.js
@@ -0,0 +1,48 @@
+const form = document.querySelector("form");
+
+// form.document.addEventListener('submit', function(e){
+//  e.prevendDefault()
+// })
+// console.log(form);
+
+// const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const height = parseInt(document.querySelector(".height").value);
+  const weight = parseInt(document.querySelector(".weight").value);
+  const result = document.querySelector("#result");
+  console.log(height, weight, result);
+
+  if (isNaN(height) || height === "") {
+    result.innerHTML = `Please enter a valid height ${height}`;
+  } else if (isNaN(weight) || weight === "") {
+    result.innerHTML = `Please enter a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //  console.log(bmi);
+    //  if (bmi <= 18.5) {
+    //    result.innerHTML = `You are underweight. BMI: ${bmi}`;
+    //  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    //    result.innerHTML = `Normal range. BMI: ${bmi}`;
+    //  } else {
+    //    result.innerHTML = `Overweight. BMI: ${bmi}`;
+    //  }
+
+    let message = "";
+    switch (true) {
+      case bmi <= 18.6:
+        message = `your are under weight = ${bmi}`;
+        break;
+      case bmi <= 24.9:
+        message = `you are fit ${bmi}`;
+        break;
+      case bmi >= 24.9:
+        message = `you are over weight ${bmi}`;
+        break;
+        default:
+         message = `try you BMI again`
+      }
+      result.innerHTML = message;
+  }
+});
