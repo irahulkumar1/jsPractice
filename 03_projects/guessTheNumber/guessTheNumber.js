78adc0b5bf20dab270f88a97fcb25fcdb2614e36
Rahul Kumar <irahulkv@outlook.com>
2023-11-30 19:01:04 +0000

    guess the number game setup

---
+++ 03_projects/guessTheNumber/guessTheNumber.js
@@ -0,0 +1,25 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.getElementById("subt");
+const useInput = document.querySelector("#guessField");
+const prevGuessSlot = document.querySelector(".guesses");
+const lastRemaining = document.querySelector(".lastResult");
+const lowOrhign = document.querySelector(".loOrHi");
+const resultParas = document.querySelector(".resultParas");
+
+console.log(useInput);
+
+
+let prevGuess = [];
+let numGuess = 1;
+let playGame = true;
+
+function validationGuess(){
+
+};
+
+function checkGuess(){
+
+};
+function displayMessage(){
+
+}
