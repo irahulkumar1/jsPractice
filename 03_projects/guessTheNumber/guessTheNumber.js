9b714abbf5d7f081aa4fb7fdde340fcb05b865b4
Rahul Kumar <irahulkv@outlook.com>
2023-12-04 20:27:44 +0000

    game done

--- 03_projects/guessTheNumber/guessTheNumber.js
+++ 03_projects/guessTheNumber/guessTheNumber.js
@@ -1,10 +1,10 @@
-const randomNumber = parseInt(Math.random() * 100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
 const submit = document.getElementById("subt");
-const userInput = document.querySelector("#guessField");
+let userInput = document.querySelector("#guessField");
 const prevGuessSlot = document.querySelector(".guesses");
 const lastRemaining = document.querySelector(".lastResult");
 const lowOrhign = document.querySelector(".loOrHi");
-const resultParas = document.querySelector(".resultParas");
+const startOver = document.querySelector(".resultParas");
 const p = document.createElement('p')
 // console.log(useInput);
 
@@ -30,7 +30,7 @@ function validationGuess(guess) {
     alert("Please enter less than 100");
   } else {
     prevGuess.push(guess);
-    if (numGuess === 11) {
+    if (numGuess === 10) { 
       displayGuess(guess);
       displayMessage(`Game Over, random number was ${randomNumber}`);
     } else {
@@ -56,8 +56,9 @@ function displayGuess(guess) {
   prevGuessSlot.innerHTML += `${guess}, `;
   numGuess++;
   lastRemaining.innerHTML = `${11 - numGuess}`;
-  console.log(guess);
+
 }
+
 function displayMessage(message) {
   lowOrhign.innerHTML = `<h2>${message}</h2>`
   console.log(message);
@@ -65,10 +66,31 @@ function displayMessage(message) {
 
 function endGame() {
   userInput.value = '';
-  userInput.setAttribute('disabled', '' );
+  userInput.setAttribute('disabled', '');
   p.classList.add('button');
-  p.innerHTML = `<h2>Start new Game</h2>`;
-  startOver.appendChild(p)
+  p.innerHTML = `<h2 id='newGame'>Start new Game</h2>`;
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
 }
 
-function newGame() {}
+
+function newGame() {
+  const newGameButton = document.querySelector('#newGame');
+  newGameButton.addEventListener("click", function(e){
+    randomNumber = parseInt(Math.random() * 100 + 1);
+    prevGuess=[];
+    numGuess = 1;
+    prevGuessSlot.innerHTML = '';
+    lastRemaining.innerHTML = '10'; // Reset remaining guesses
+    userInput.removeAttribute("disabled");
+
+    
+    if (startOver.contains(p)) {
+      startOver.removeChild(p);
+    }
+    
+    playGame = true;
+  });
+}
+
