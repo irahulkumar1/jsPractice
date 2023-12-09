5f70fe676bafccfdd0887a7c33b512e9ec97d13b
Rahul Kumar <irahulkv@outlook.com>
2023-12-09 19:09:58 +0000

    auto background changer

--- 05_asyncAwait/index.js
+++ 05_asyncAwait/index.js
@@ -1,7 +1,19 @@
 // Async Await 
 
-const syaRahul = function (){
-    console.log('Hi Rahul')
-}
+// const syaRahul = function (){
+//     console.log('Hi Rahul')
+// }
 
-setTimeout(syaRahul, 1000);
+// setTimeout(syaRahul, 1000);
+
+const startBtn = document.getElementById ('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+
+startBtn.addEventListener('click',() => {
+    console.log("click");
+})
+stopBtn.addEventListener('click',() => {
+    console.log("click");
+})
+
+
