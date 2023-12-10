4c32bad20b9ebae66dfcfb2066aba7ae78f08669
Rahul Kumar <irahulkv@outlook.com>
2023-12-10 20:04:44 +0000

    color changer game

--- 05_asyncAwait/index.js
+++ 05_asyncAwait/index.js
@@ -9,11 +9,66 @@
 const startBtn = document.getElementById ('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 
+
+const randomColor = ()=>{
+    const hex = '0123456789ABCDEF';
+    let color ='#';
+    for (let i=0; i<6 ; i++)   {
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+randomColor();
+let intervelId;
+function startColoreChanging(){
+    intervelId =  setInterval(()=>{
+        document.body.style.backgroundColor= randomColor()
+    },1000)
+}
+
 startBtn.addEventListener('click',() => {
-    console.log("click");
+    startColoreChanging()
 })
 stopBtn.addEventListener('click',() => {
     console.log("click");
+    clearInterval(intervelId);
 })
 
 
+// const colorChanger = {
+//   intervalId: null,
+
+//   getRandomColor() {
+//     const hex = '0123456789ABCDEF';
+//     let color = '#';
+//     for (let i = 0; i < 6; i++) {
+//       color += hex[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+//   },
+
+//   startColorChanging() {
+//     this.intervalId = setInterval(() => {
+//       document.body.style.backgroundColor = this.getRandomColor();
+//     }, 1000);
+//   },
+
+//   stopColorChanging() {
+//     clearInterval(this.intervalId);
+//   },
+
+//   initialize() {
+//     const startBtn = document.getElementById('startBtn');
+//     const stopBtn = document.getElementById('stopBtn');
+
+//     startBtn.addEventListener('click', () => {
+//       this.startColorChanging();
+//     });
+
+//     stopBtn.addEventListener('click', () => {
+//       this.stopColorChanging();
+//     });
+//   },
+// };
+
+// colorChanger.initialize();
