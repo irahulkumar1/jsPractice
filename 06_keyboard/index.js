37699cd825ec072adb373cb24fd48ff69d7d7e2b
Rahul Kumar <irahulkv@outlook.com>
2023-12-17 17:42:02 +0000

    keyboard project finished

--- 06_keyboard/index.js
+++ 06_keyboard/index.js
@@ -1,6 +1,19 @@
 //app to create to detct keyboard eventes
 const insert = document.getElementById("insert");
 
-window.addEventListener('keydown', (e)=>{
-    insert.innerHTML = `div.color`
-})
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `<div class= 'color'>  
+    <table>
+  <tr>
+    <th>Key</th>
+    <th>Key code</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? "space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    </div>`;
+});
